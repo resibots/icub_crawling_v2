8d38f9bbd9a2a0942e43045f64945240f22b8ec2
Jean-Baptiste Mouret <mouret@isir.upmc.fr>
2015-07-30 15:13:35 +0200

    reading config file

--- modules/cpg/main.cpp
+++ modules/cpg/main.cpp
@@ -1,6 +1,12 @@
 // -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
 
-#include <stdio.h>
+#include <cstdio>
+#include <string>
+#include <memory>
+#include <vector>
+#include <map>
+#include <cassert>
+
 #include <yarp/os/Network.h>
 #include <yarp/dev/ControlBoardInterfaces.h>
 #include <yarp/dev/PolyDriver.h>
@@ -8,16 +14,98 @@
 #include <yarp/sig/Vector.h>
 #include <yarp/os/RFModule.h>
 
-#include <string>
+
+#include <yarp/os/RateThread.h>
 
 using namespace yarp::dev;
 using namespace yarp::sig;
 using namespace yarp::os;
 
+
+class CrawlerThread : public yarp::os::RateThread {
+public:
+  CrawlerThread(int period) : yarp::os::RateThread(period) { }
+  void run() { /* TODO */ }
+  virtual bool threadInit() { return true; }
+  virtual void threadRelease() { /* TODO */ }
+  bool configure(yarp::os::ResourceFinder &rf);
+
+protected:
+  std::map<std::string, std::vector<double> > _parts;
+  std::string _robot_name;
+};
+
+
+class CrawlerModule : public yarp::os::RFModule {
+public:
+  CrawlerModule(){}
+  virtual ~CrawlerModule() {}
+  virtual double getPeriod() { return 1.0; }
+  virtual bool close() {
+    _thread->stop();
+    return true;
+  }
+
+  bool configure(yarp::os::ResourceFinder &rf);
+  bool interruptModule() { return true; }
+  bool updateModule() { return true; }
+
+protected:
+  std::shared_ptr<CrawlerThread> _thread; /**< Periodic generator thread */
+  std::string _module_name;
+};
+
+bool CrawlerModule::configure(yarp::os::ResourceFinder &rf) {
+  _module_name = rf.check("name", Value("crawler"), "module name (string)").asString();
+  setName(_module_name.c_str());
+
+
+  _thread = std::shared_ptr<CrawlerThread>(new CrawlerThread(1.0));
+  _thread->configure(rf);
+  _thread->start();
+  return true;
+}
+
+
+bool CrawlerThread::configure(yarp::os::ResourceFinder& rf) {
+  _robot_name = rf.check("robot", Value("icub"), "Robot name (string)").asString();
+  std::cout<< "robot: "<< _robot_name << std::endl;
+
+  std::string fname = rf.check("initial_pos", Value("initial_pos.ini"), "initial pos filename (string)").asString();
+  fname = (rf.findFile(fname.c_str()));
+  Property robot_properties;
+  if (!robot_properties.fromConfigFile(fname.c_str()))
+  {
+    std::cerr << "CrawlerModule: unable to read initial pos file ["
+    << fname <<" ]" << std::endl;
+    return false;
+  }
+  Bottle& p = robot_properties.findGroup("part_names");
+  assert(!p.isNull());
+  for (int i = 1; i < p.size(); ++i)
+    _parts[p.get(i).toString()] = std::vector<double>();
+
+  for (auto& s : _parts) {
+    std::cout << "part:[" << s.first <<"]";
+    Bottle& b = robot_properties.findGroup(s.first);
+    for (int i = 1; i < b.size(); ++i)
+      _parts[s.first].push_back(b.get(i).asDouble());
+    std::cout<<"   =>"<<_parts[s.first].size()<< " joints." << std::endl;
+  }
+  return true;
+}
+
+
 int main(int argc, char *argv[])
 {
   ResourceFinder rf;
+  rf.setDefaultContext("crawling");
+  rf.setDefaultConfigFile("config_cpg-default.ini");
+  rf.configure(argc, argv);
 
+  CrawlerModule crawler;
+  crawler.configure(rf);
+  exit(0);
 
   Network yarp;
 
